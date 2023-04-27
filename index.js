2434f7cb7d618df8c50dcd3dea165daf3fecf854
Sumit <sumitbattul243@gmail.com>
2023-04-27 18:25:04 +0530

    testing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var express = require(‘express’);
 var app = express();                      //Respond with "hello world" for requests that hit our root "/"
 app.get(‘/’, function (req, res) {
- res.send(‘hey!! there’);
+ res.send(‘hello World’);
 });                                       //listen to port 80 by default
 app.listen(process.env.PORT || 80);
 module.exports = app;
